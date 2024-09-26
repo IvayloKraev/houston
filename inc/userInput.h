04a6946ccf1ee55f36bbb3a5a8b97764cdc35a56
IvayloKraev <ivaylokraev@protonmail.com>
2024-09-26 06:22:56 +0300

    Add basic user input logic

--- inc/userInput.h
+++ inc/userInput.h
@@ -1,12 +1,36 @@
 #ifndef HOUSTON_USERINPUT_H
 #define HOUSTON_USERINPUT_H
 
-#define START_MOTORS_PIN    4
-#define STOP_MOTORS_PIN     5
-#define LEFT_TURN_PIN       6
-#define RIGHT_TURN_PIN      7
+#include <stdint.h>
+#include "stdbool.h"
 
-#define SPEED_CTRL_PIN      9
+#include <pico/stdio.h>
+
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#define START_MOTORS_PIN    2
+#define STOP_MOTORS_PIN     3
+#define LEFT_TURN_PIN       4
+#define RIGHT_TURN_PIN      5
+
+#define SPEED_CTRL_ADC      2
+#define SPEED_CTRL_PIN      (SPEED_CTRL_ADC + 26)
+
+
+typedef struct {
+    bool startMotors;
+    bool stopMotors;
+    bool leftTurn;
+    bool rightTurn;
+    uint16_t speed;
+} rawUserInput_t;
+
+void houston_userInput_init(QueueHandle_t);
 
 void houston_userInput_getState();
 
--- src/userInput.c
+++ src/userInput.c
@@ -1,2 +1,57 @@
 #include "userInput.h"
 
+rawUserInput_t userInput_raw = {};
+QueueHandle_t userInput_rawCommandQueue = NULL;
+
+void houston_userInput_init(QueueHandle_t newRawCommandQueue) {
+
+    if(newRawCommandQueue == NULL) {
+        stdio_printf("Raw Command Queue is NULL\n");
+        return;
+    }
+
+    userInput_rawCommandQueue = newRawCommandQueue;
+
+    gpio_init(START_MOTORS_PIN);
+    gpio_set_dir(START_MOTORS_PIN, GPIO_IN);
+
+    gpio_init(STOP_MOTORS_PIN);
+    gpio_set_dir(STOP_MOTORS_PIN, GPIO_IN);
+
+    gpio_init(LEFT_TURN_PIN);
+    gpio_set_dir(LEFT_TURN_PIN, GPIO_IN);
+
+    gpio_init(RIGHT_TURN_PIN);
+    gpio_set_dir(RIGHT_TURN_PIN, GPIO_IN);
+
+    adc_init();
+    adc_gpio_init(SPEED_CTRL_PIN);
+}
+
+void houston_userInput_getState() {
+    while (1) {
+        userInput_raw.startMotors = gpio_get(START_MOTORS_PIN);
+        userInput_raw.stopMotors = gpio_get(STOP_MOTORS_PIN);
+        userInput_raw.leftTurn = gpio_get(LEFT_TURN_PIN);
+        userInput_raw.rightTurn = gpio_get(RIGHT_TURN_PIN);
+
+        adc_select_input(SPEED_CTRL_ADC);
+        userInput_raw.speed = adc_read();
+
+        xQueueSendToBack(userInput_rawCommandQueue, &userInput_raw, (TickType_t) 10);
+
+        stdio_printf(
+            "startMotors: %d\n"
+            "stopMotors: %d\n"
+            "leftTurn: %d\n"
+            "rightTun: %d\n"
+            "speed: %d\n",
+            userInput_raw.startMotors,
+            userInput_raw.stopMotors,
+            userInput_raw.leftTurn,
+            userInput_raw.rightTurn,
+            userInput_raw.speed);
+
+        vTaskDelay(100);
+    }
+};
