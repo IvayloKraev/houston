7f4886c32ba7c4ac1503ccc809844882dc0a7094
IvayloKraev <ivaylokraev@protonmail.com>
2024-10-21 18:42:49 +0300

    Add status component

---
+++ inc/status.h
@@ -0,0 +1,22 @@
+#ifndef HOUSTON_STATUS_H
+#define HOUSTON_STATUS_H
+
+#include "stdint.h"
+
+#include "FreeRTOS.h"
+#include "event_groups.h"
+
+#define HOUSTON_STATUS_WIFI_DONE (1 << 0)
+
+#define HOUSTON_STATUS_WAIT_WIFI_DONE() houston_status_waitStatus(HOUSTON_STATUS_WIFI_DONE)
+#define HOUSTON_STATUS_SET_WIFI_DONE() houston_status_setStatus(HOUSTON_STATUS_WIFI_DONE)
+
+
+void houston_status_init();
+
+void houston_status_waitStatus(EventBits_t);
+
+void houston_status_setStatus(EventBits_t);
+
+
+#endif
---
+++ src/status.c
@@ -0,0 +1,21 @@
+#include "status.h"
+
+EventGroupHandle_t houston_status_eventGroup;
+
+void houston_status_init() {
+    houston_status_eventGroup = xEventGroupCreate();
+}
+
+void houston_status_waitStatus(EventBits_t bitsToWait) {
+    do{
+        EventBits_t wifiBit = xEventGroupWaitBits(houston_status_eventGroup, bitsToWait, pdFALSE, pdTRUE, portMAX_DELAY);
+
+        if(wifiBit != 0) return;
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }while(1);
+}
+
+void houston_status_setStatus(EventBits_t bits) {
+    xEventGroupSetBits(houston_status_eventGroup, bits);
+}
