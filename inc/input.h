bbbccec4d6313fc3736a607dfa90edb17de8ba3c
IvayloKraev <ivaylokraev@protonmail.com>
2025-06-10 23:58:32 +0300

    Create input struct

--- inc/input.h
+++ inc/input.h
@@ -27,7 +27,17 @@
 #define SPEED_ADC           2
 #define SPEED_PIN           (SPEED_ADC + 26)
 
-hcst_ERROR_t houston_input_init();
+typedef struct {
+    bool dragEnable;
+    bool forward;
+    bool backward;
+    bool leftSpin;
+    bool rightSpin;
+    bool joystickBtn;
+    uint16_t joystickX;
+    uint16_t joystickY;
+    uint16_t speed;
+} inputData_t;
 
 hcst_ERROR_t houston_input_tick(hcst_message_t);
 
