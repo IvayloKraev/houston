cfb15f01a1ebfcc848e600b5fad30eaaa623bcd4
IvayloKraev <ivaylokraev@protonmail.com>
2025-06-13 23:53:22 +0300

    Remove houston result

--- inc/common.h
+++ inc/common.h
@@ -3,14 +3,6 @@
 
 #include <stdint.h>
 
-typedef enum {
-    houston_OK = 1,
-    houston_WARN = 0,
-    houston_ERR = -1,
-} houston_RESULT;
-
-typedef BaseType_t (*sendMessageFn_t)(hcst_message_t);
-
 inline uint8_t houston_common_mapToUint8(unsigned int min, unsigned int max, unsigned int val) {
     return min == max ? 0 : (uint8_t) (((val < min ? min : (val > max ? max : val)) - min) * 255 / (max - min));
 }
